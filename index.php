37c9cfbb515e472f5bd79aa3f016d670d3c8c2bb
SnowyyCodes <76702533+SnowyyCodes@users.noreply.github.com>
2021-12-14 12:46:18 -0700

    Update index.php

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ function loginForm(){
                         success: function (html) {
                             $("#chatbox").html(html); //Insert chat log into the #chatbox div
  
-                            //Auto-scroll           
+                            //Auto-scroll s          
                             var newscrollHeight = $("#chatbox")[0].scrollHeight - 20; //Scroll height after the request
                             if(newscrollHeight > oldscrollHeight){
                                 $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
